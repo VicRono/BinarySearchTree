f2266a3a1aaa9a5275ead132efc4445e3e85c46a
Victor Rono <v.rono@icloud.com>
2019-03-28 14:34:26 -0500

    able to traverse left and right

--- BinarySearchTree/BST.cs
+++ BinarySearchTree/BST.cs
@@ -15,6 +15,42 @@ namespace BinarySearchTree
             root = null;
         }
 
+        public void Add(int dataToAdd)
+        {
+            Node toAdd = new Node((int)dataToAdd);
 
+            if(root == null)
+            {
+                root = toAdd;
+                return;
+            }
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (dataToAdd < current.data)
+                    {
+                        if(current == null)
+                        {
+                            parent.left = toAdd;
+                            break;
+                        }
+
+                    }
+                    else
+                    {
+                        current = current.right;
+                        if (current == null)
+                        {
+                            parent.right = toAdd;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
