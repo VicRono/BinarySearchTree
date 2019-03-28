a11c010c2f55cc1b8809c69c54d83ef3141d8526
Victor Rono <v.rono@icloud.com>
2019-03-28 14:45:09 -0500

    added random number generator and for loop in program.cs

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -10,6 +10,16 @@ namespace BinarySearchTree
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            BST binarysearch = new BST();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int randomNumber = random.Next(1, 101);
+                binarysearch.Add(randomNumber);
+                Console.WriteLine($"#{i} is {randomNumber}");
+            }
+            Console.ReadLine();
         }
     }
 }
